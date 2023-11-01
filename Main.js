3727796bb09b853fa3d91b8f8a66dd4902657b3a
Philip Bendiksen <philipbendiksen@gmail.com>
2023-11-01 10:16:04 +0100

    First try

---
+++ Main.js
@@ -0,0 +1,17 @@
+
+ 
+var value = 0; 
+while (value < 10) { 
+
+   console.log(value); 
+    value++; 
+  }
+
+function addTwoValues (firstValue, secondValue) { 
+    return (firstValue + secondValue)
+
+}
+
+var addValues = addTwoValues (1, 4); 
+
+
