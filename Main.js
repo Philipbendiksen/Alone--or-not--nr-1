9267360f75c62322a27b903744f45f1751e33243
Philip Bendiksen <philipbendiksen@gmail.com>
2023-11-07 14:28:08 +0100

    Made a start page and added several buttons / link

--- Main.js
+++ Main.js
@@ -1,17 +1,26 @@
 
- 
-var value = 0; 
-while (value < 10) { 
 
-   console.log(value); 
-    value++; 
-  }
+var value = 0;
+while (value < 10) {
 
-function addTwoValues (firstValue, secondValue) { 
-    return (firstValue + secondValue)
+  console.log(value);
+  value++;
+}
+
+function addTwoValues(firstValue, secondValue) {
+  return (firstValue + secondValue)
 
 }
 
-var addValues = addTwoValues (1, 4); 
+var addValues = addTwoValues(1, 4);
+
+
+let x = 18;
+if (x === 2) {
+  console.log("x är lika med 2");
+} else {
+  console.log("x är lika med 2 (jag ljuger)");
+}
+
 
 
