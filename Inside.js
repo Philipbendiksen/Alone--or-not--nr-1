607732cc6e441558f1f3b875593fef16c38a68e1
Philip Bendiksen <philipbendiksen@gmail.com>
2023-11-13 17:36:21 +0100

    Done more to buttons

---
+++ Inside.js
@@ -0,0 +1,32 @@
+
+/*  Funktion knapp ved */
+
+let knappFörVed = document.getElementById('knappFörVed');
+let vedCountElement = document.getElementById('vedCount');
+
+let vedCount = 0;
+
+
+
+
+/*  Funktion knapp yxor  */
+
+let knappFörYxa = document.getElementById('knappFörYxa');
+let yxaCountElement = document.getElementById('yxaCount');
+
+let yxaCount = 0;
+
+
+
+
+/*  funktion knapp svampar  */
+
+let knappFörSvamp = document.getElementById('knappFörSvamp');
+let svampCountElement = document.getElementById('svampCount');
+
+let svampCount = 0;
+
+
+
+
+
